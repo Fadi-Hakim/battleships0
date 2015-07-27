5442a87298aa47e1552ccebfb20f729e60f8a19e
Christopher Alcock <christopher.alcock@gmail.com>
2015-07-27 12:13:19 +0100

    place method does something

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,13 @@
 class Board
+
+attr_reader :grid
+
+  def initialize
+    @grid = []
+  end
+
+  def place_ship(ship)
+    grid << ship
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,4 +2,18 @@ require 'board'
 
 describe Board do
 
+  it "has a grid" do
+    expect(subject.grid).to eq []
+  end
+
+  it "responds to place_ship method" do
+    expect(subject).to respond_to(:place_ship).with(1).argument
+  end
+
+  describe "#place_ship" do
+    it "puts the ship into the grid" do
+      subject.place_ship(:ship)
+      expect(subject.grid).to include(:ship)
+    end
+  end
 end
