21305685abfe6bb4979343768a2008f9db46fed2
Christopher Alcock <christopher.alcock@gmail.com>
2015-07-27 13:38:50 +0100

    added rough file to test 2d arrays

---
+++ rough.rb
@@ -0,0 +1,16 @@
+row_1 = [:x, :x, :x]
+row_2 = [:x, :x, :x]
+row_3 = [:x, :x, :x]
+
+grid = [row_1, row_2, row_3]
+
+grid[0][0] = :F
+grid[1][0] = :F
+grid[2][0] = :F
+
+def
+
+
+puts row_1.to_s
+puts row_2.to_s
+puts row_3.to_s
