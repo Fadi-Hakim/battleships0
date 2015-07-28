464b31865a1b4f18fbad346d51c288fdb26f5d36
Fadi-Hakim <Fadihakim@msn.com>
2015-07-28 16:36:03 +0100

    ship size started

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,8 +1,16 @@
 class Ship
-  attr_reader :coordinate
-  def initialize
+  SHIP_TYPES = {destroyer: 2, submarine: 3}
+
+  def self.destroyer
+    Ship.new(SHIP_TYPES[__method__])
+  end
+
+  attr_reader :coordinate, :size
+
+  def initialize(size)
     @placed = false
     @coordinate
+    @size = size
   end
 
   def placed?
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,23 +3,37 @@ require 'ship'
 describe Ship do
 
   it "starts game not placed" do
-    expect(subject).not_to be_placed
+    ship = described_class.destroyer
+    expect(ship).not_to be_placed
   end
 
   it "is placed after being placed" do
-    subject.place :A1
-    expect(subject).to be_placed
+    ship = described_class.destroyer
+    ship.place :A1
+    expect(ship).to be_placed
+  end
+
+  it "is a destroyer size 2" do
+    destroyer = described_class.destroyer
+    expect(destroyer.size).to eq 2
+  end
+
+  it "is a submarine size 3" do
+    submarine = described_class.submarine
+    expect(submarine.size).to eq 3
   end
 
   describe "#place" do
     it "has a coordinate after being placed" do
-      subject.place :A1
-      expect(subject.coordinate).to eq :A1
+      ship = described_class.destroyer
+      ship.place :A1
+      expect(ship.coordinate).to eq :A1
     end
 
     it 'can only be placed one' do
-      subject.place :A1
-      expect { subject.place :A1 }.to raise_error "ship has already been placed"
+      ship = described_class.destroyer
+      ship.place :A1
+      expect { ship.place :A1 }.to raise_error "ship has already been placed"
     end
   end
 end
